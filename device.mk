a3a00517af800c706e03d5f2f5b9d2bd6396492e
mikecriggs <michael.s.corigliano@gmail.com>
2017-05-21 23:36:18 -0400

    TEMP: remove keyhandler
    Change-Id: I72094b86941f9969f9060e93355e70f5b31d3924

--- device.mk
+++ device.mk
@@ -149,11 +149,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/goodix.kl:system/usr/keylayout/goodix.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
-# Keyhandler
-PRODUCT_PACKAGES += \
-    CMActions \
-    com.cyanogenmod.keyhandler
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8916
